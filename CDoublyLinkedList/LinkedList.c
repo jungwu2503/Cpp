6a0acd6cccdb0b44c2313d658a06a3c7a2c76b11
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2023-03-29 13:19:18 +0900

    Add files via upload

---
+++ CDoublyLinkedList/LinkedList.c
@@ -0,0 +1,387 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "LinkedList.h"
+
+Node *allocNode(int data)
+{
+	Node *tmp = (Node *)malloc(sizeof(Node)); 
+
+	tmp->data = data;
+	tmp->prev = tmp;
+	tmp->next = tmp;
+
+	return tmp;
+}
+
+void insert(Node *node, Node *chain)
+{
+	node->prev = chain->prev;
+	node->next = chain;
+	chain->prev->next = node;
+	chain->prev = node;
+}
+
+void append(Node *node, Node *chain)
+{
+	node->prev = chain;
+	node->next = chain->next;
+	chain->next->prev = node;
+	chain->next = node;
+}
+
+void removeNode(Node *node)
+{
+	//printf("node->data = %d\n", node->data);
+	node->prev->next = node->next;
+	node->next->prev = node->prev;
+	free(node);
+}
+
+/*main()
+{
+	Node *p;
+	Node *q;
+	Node *tmp;
+
+	q = allocNode(7);
+
+	tmp = q;
+	printf("%d ", tmp->data);
+	while (tmp->next != q) {		
+		tmp = tmp->next;
+		printf("%d ", tmp->data);
+	}
+	printf("\n");
+
+	p = allocNode(5);
+	insert(p, q);
+	tmp = q;
+	printf("%d ", tmp->data);
+	while (tmp->next != q) {
+		tmp = tmp->next;
+		printf("%d ", tmp->data);
+	}
+	printf("\n");
+
+	p = allocNode(9);
+	insert(p, q);
+	tmp = q;
+	printf("%d ", tmp->data);
+	while (tmp->next != q) {
+		tmp = tmp->next;
+		printf("%d ", tmp->data);
+	}
+	printf("\n");
+
+	p = allocNode(8);
+	insert(p, q);
+	tmp = q;
+	printf("%d ", tmp->data);
+	while (tmp->next != q) {
+		tmp = tmp->next;
+		printf("%d ", tmp->data);
+	}
+	printf("\n");
+}*/
+
+void initList(LinkedList *l) 
+{
+	l->header = NULL;
+	l->count = 0;
+}
+
+void disposeList(LinkedList *l) 
+{
+	Node *tmp;
+	if (l->header == NULL) return;
+	while (l->header->next != l->header) {
+		tmp = l->header->next;
+		removeNode(tmp);
+	}
+	removeNode(l->header);
+}
+
+void addFirst(LinkedList *l, int x)
+{
+	add(l, x);
+	l->header = l->header->prev;
+
+}
+
+void add(LinkedList *l, int x) 
+{
+	Node *tmp;
+
+	l->count = l->count + 1;
+	if (l->header == NULL) {
+		l->header = allocNode(x);
+		return;
+	}
+	tmp = allocNode(x);
+	insert(tmp, l->header);
+}
+
+void addLast(LinkedList *l, int x)
+{
+	add(l, x);
+}
+
+void addAt(LinkedList *l, int index, int x) 
+{
+	Node *tmp;
+	Node *newNode;
+	int i;
+
+	if (index == 0) {
+		addFirst(l, x);
+		return;
+	}
+	tmp = l->header;
+	for (i = 0; i < index; i++) {
+		tmp = tmp->next;
+	}
+	newNode = allocNode(x);
+	insert(newNode, tmp);
+}
+
+void addAllAt(LinkedList *l1, LinkedList *l2, int index)
+{
+	Node *tmp;
+	Node *tmp2;
+	Node *newNode;
+	int l2Count;
+	int i;
+
+	tmp = l1->header;
+	for (i = 0; i < index; i++) {
+		tmp = tmp->next;
+	}
+	tmp2 = l2->header;
+	l2Count = l2->count;
+	while (l2Count-- > 0) {
+		newNode = allocNode(tmp2->data);
+		insert(newNode, tmp);
+		tmp2 = tmp2->next;
+		l1->count++;
+	}
+	if (index == 0) {
+		for (i = 0; i < l2->count; i++) {
+			tmp = tmp->prev;
+		}
+		l1->header = tmp;
+	}
+}
+
+void print(LinkedList *l) 
+{
+	Node *tmp;
+	
+	if (l->header == NULL) {
+		printf("[]\n");
+		return;
+	}
+	tmp = l->header;
+	printf("[%d", tmp->data);
+	while (tmp->next != l->header) {
+		tmp= tmp->next;
+		printf(" %d", tmp->data);
+	}
+	printf("]\n");
+}
+
+int size(LinkedList *l)
+{
+	return l->count;
+}
+
+void clear(LinkedList *l)
+{
+	Node *tmp;
+	if (l->header == NULL) return;
+	while (l->header->next != l->header) {
+		tmp = l->header->next;
+		removeNode(tmp);
+	}
+	l->header = NULL;
+	l->count = 0;
+}
+
+LinkedList *clone(LinkedList *org)
+{
+	LinkedList *newList;
+	Node *tmp;
+	Node *newNode;
+	int i;
+
+	newList = (LinkedList *)malloc(sizeof(LinkedList));
+	initList(newList);
+
+	tmp = org->header;
+	for (i = 0; i < org->count; i++) {
+		add(newList, tmp->data);
+		tmp = tmp->next;
+	}
+	newList->count = org->count;
+
+	return newList;
+}
+
+char contains(LinkedList *l, int x) 
+{
+	int i;
+	Node *tmp;
+
+	tmp = l->header;
+	for (i = 0; i < l->count; i++) {
+		if (tmp->data == x) return TRUE;
+		tmp = tmp->next;
+	}
+
+	return FALSE;
+}
+
+int get(LinkedList *l, int index) 
+{
+	int i;
+	Node *tmp;
+
+	if (l->header == NULL) return -1;
+	tmp = l->header;
+	for (i = 0; i < index; i++) {
+		tmp = tmp->next;
+	}
+	return tmp->data;
+}
+
+int getFirst(LinkedList *l) 
+{
+	if (l->header == NULL) return -1;
+	return l->header->data;
+}
+
+int getLast(LinkedList *l) 
+{
+	if (l->header == NULL) return -1;
+	return l->header->prev->data;
+}
+
+int indexOf(LinkedList *l, int x) 
+{
+	Node *tmp;
+	int pos;
+	int i;
+
+	if (l->header == NULL) return -1;
+	tmp = l->header;
+	pos = 0;
+	for (i = 0; i < l->count; i++) {
+		if (tmp->data == x) return pos;
+		tmp = tmp->next;
+		pos++;
+	}
+	return -1;
+}
+
+int lastIndexOf(LinkedList *l, int x)
+{
+	Node *tmp;
+	int pos;
+	int i;
+	int value = -1;
+
+	if (l->header == NULL) return value;
+	pos = 0;
+	tmp = l->header;
+	for (i = 0; i < l->count; i++) {
+		if (tmp->data == x) value = pos;
+		tmp = tmp->next;
+		pos++;
+	}
+
+	return value;
+}
+
+int element(LinkedList *l) 
+{
+	if (l->header == NULL) return -1;
+	return l->header->data;
+}
+
+void set(LinkedList *l, int index, int x) 
+{
+	Node *tmp;
+	int i;
+
+	if (l->header == NULL) {
+		printf("List is empty\n");	
+		return;
+	}
+	tmp = l->header;
+	for (i = 0; i < index; i++) {
+		tmp = tmp->next;
+	}
+	tmp->data = x;
+}
+
+void removeData(LinkedList *l, int x)
+{
+	Node *tmp;
+	int i;
+
+	if (l->header == NULL) {
+		printf("List is empty\n");	
+		return;
+	}
+	tmp = l->header;
+	l->count = l->count - 1;
+	for (i = 0; i < l->count; i++) {
+		if (tmp->data == x) {
+			removeNode(tmp);
+			return;
+		}
+		tmp = tmp->next;
+	}
+
+	printf("No such element\n");	
+}
+
+void removeAt(LinkedList *l, int index)
+{
+	Node *tmp;
+	int i;
+
+	if (l->header == NULL) {
+		printf("List is empty\n");	
+		return;
+	}
+	tmp = l->header;
+	l->count = l->count - 1;
+	/*if (index == 0) {
+		tmp->prev->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+		free(tmp);
+		return;
+	}*/
+	for (i = 0; i < index; i++) {
+		tmp = tmp->next;
+	}
+	removeNode(tmp);
+}
+
+int *toArray(LinkedList *l)
+{
+	int *arr;
+	Node *tmp;
+	int i;
+
+	if (l->count == 0) return NULL;
+	arr = (int *)malloc(sizeof(int) * size(l));
+	tmp = l->header;
+
+	for (i = 0; i < l->count; i++) {
+		arr[i] = tmp->data;
+		tmp = tmp->next;
+	}
+
+	return arr;
+}
---
+++ CDoublyLinkedList/test.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "LinkedList.h"
+
+//doubly LinkedLIst -> Singly LinkedList 단점 보완(add, traverse 역순)
+
+main()
+{
+	LinkedList list1;
+	LinkedList list2;
+	LinkedList *list3;
+	int *arr;
+	int i;
+
+	initList(&list1);
+	initList(&list2);
+
+	printf("list1 = ");
+	print(&list1);
+	printf("list2 = ");
+	print(&list2);
+
+	add(&list1, 10);
+	add(&list1, 20);
+	add(&list1, 30);
+	add(&list1, 40);
+
+	add(&list2, 100);
+	add(&list2, 200);
+
+	printf("list1 = ");
+	print(&list1);
+	printf("list2 = ");
+	print(&list2);
+
+	//addAt(&list1, 0, 7);
+	//print(&list1);
+
+	addAllAt(&list1, &list2, 5);
+	printf("list1 = ");
+	print(&list1);
+
+	//clear(&list1);
+	//print(&list1);
+
+	list3 = clone(&list1);
+	printf("list3 = ");
+	print(list3);
+
+	//printf("%d\n", contains(&list1,110));
+	//printf("%d\n", get(&list1,7));
+	//printf("%d\n", getFirst(&list1));
+	//printf("%d\n", getLast(&list1));
+	//printf("%d\n", indexOf(&list1, 50));
+	//printf("%d\n", lastIndexOf(&list1, 7));
+
+	set(&list1, 2, 77);
+	printf("list1 = ");
+	print(&list1);
+
+	removeData(&list1, 77);
+	printf("list1 = ");
+	print(&list1);
+	
+	//removeAt(&list1, 0); // <====================
+	//printf("list1 = ");
+	//print(&list1);
+
+	arr = toArray(&list1);
+	printf("[ ");
+	for (i = 0; i < size(&list1); i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("]\n");
+
+	disposeList(&list1);
+	/*
+	add(&list2, 100);
+	add(&list2, 200);
+	add(&list2, 300);
+
+	printf("get 0 %d\n", get(&list1, 0));
+	printf("getFirst %d\n", getFirst(&list1));
+	printf("getLast %d\n", getLast(&list1));
+	printf("list1 = ");
+	print(&list1);
+
+	printf("list2 = ");
+	print(&list2);
+
+	//printf("%d\n", get(&list1, 0));
+	//printf("%d\n", element(&list1));
+	//printf("%d\n", getFirst(&list1));
+	//printf("%d\n", getLast(&list1));
+	printf("%d\n", lastIndexOf(&list1, 30));
+	
+	set(&list1, 3, 700);
+
+	//removeAt(&list1, 2);
+	//removeAt(&list1, 0);
+	removeData(&list1, 700);
+	
+	printf("list1 = ");
+	print(&list1);
+
+	arr = toArray(&list1);
+	for (i = 0; i < size(&list1); i++) {
+		printf("%d ", arr[i]);
+	}*/
+}
